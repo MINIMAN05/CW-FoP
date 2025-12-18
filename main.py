30e12fec492bc6d042cd9e64cbc875a33d2c0574
MINIMAN05 <biloladxam237899ba@gmail.com>
2025-12-18 12:54:19 +0500

    display_books function is added

--- main.py
+++ main.py
@@ -1,70 +1,73 @@
 books = []
 
 def add_book():
-    try:
-        id = int(input("Enter ID of the book: "))
-    except Exception as e:
-        print(e)
-        return # return 
-            
-    try:
+    while True:
+        try:
+            id = int(input("Enter ID of the book: "))
+        except Exception as e:
+            print(e)
+            return # return 
+
         title = input("Enter title of the book: ")
-    except Exception as e:
-        print(e)
-        return
-        
-        
-    try:
-        author = input("Enter author of the book: ")
-    except Exception as e:
-        print(e)
-        return
-        
-        
-    try:
-        genre = input("Enter genre of the book: ")
-    except Exception as e:
-        print(e)
-        return
-        
-        
-    try:
-        year = int(input("Enter published year of the book: "))
-    except Exception as e:
-        print(e)
-        return
-        
+
+        author = input("Enter author of the book: ").title()
+
+        genre = input("Enter genre of the book: ").capitalize()
             
-    try:
-        price = float(input("Enter published price of the book: "))
-    except Exception as e:
-        print(e)
-        return
+        try:
+            year = int(input("Enter published year of the book: "))
+            
+        except Exception as e:
+            print(e)
+            return
+            
+                
+        try:
+            price = float(input("Enter price of the book: "))
+            
+        except Exception as e:
+            print(e)
+            return
+        book = {
+            'id':id,
+            'title':title,
+            'author':author,
+            'genre':genre,
+            'year':year,
+            'price':price,
+            }
+        books.append(book)
+            
+        
+ 
+        choose_to_stop = input("Print exit, if you finished adding books. Just press Enter if you want to keep adding: ")
+
+        if choose_to_stop == 'exit':
+            break
+
+
+
+    main_menu()
+
+def display_books():
+    print("Id", 3*' ',"Title", 3*' ',"Author", 3*' ',"Genre", 3*' ',"Year", 3*' ',"Price", 3*' ')
+    for i in books:
+        print(i['id'], i['title'], i['author'], i['genre'], i['year'], i['price'])
+        
+    # print(books)
+    main_menu()
     
-    try:
-        choose_to_stop = input("Print exit, if you finished adding books. Just press Enter if you want to keep adding")
-    except Exception as e:
-        print(e)
-    if choose_to_stop == 'exit':
-        pass
-    book = {
-        'id':id,
-        'title':title,
-        'author':author,
-        'genre':genre,
-        'year':year,
-        'price':price,
-        }
-    books.append(book)
-    print(books)
 
 def main_menu():
-    print(30*'-', "MAIN MENU", 30*'-')
-    print('1. Add a book')
-    option = int(input("Enter a option that you want to "))
-    while True:
+    while True:    
+        print(30*'-', "MAIN MENU", 30*'-')
+        print('1. Add a book')
+        print('2. Show a book')
+        option = int(input("Enter a option that you want to "))
         if option == 1:
             add_book()
+        elif option == 2:
+            display_books()
             
-
 main_menu()
+
