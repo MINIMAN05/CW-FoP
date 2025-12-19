ce5a627e43817da68fae602ca25ca2473065a06c
MINIMAN05 <biloladxam237899ba@gmail.com>
2025-12-19 17:35:44 +0500

    borrow() function was added

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def add_book():
             return
             
                 
-        try:
-            price = float(input("Enter price of the book: "))
+        # try:
+        #     price = float(input("Enter price of the book: "))
             
         except Exception as e:
             print(e)
@@ -34,7 +34,7 @@ def add_book():
             'author':author,
             'genre':genre,
             'year':year,
-            'price':price,
+            'is_available': 'Yes'
             }
         books.append(book)
             
@@ -50,24 +50,57 @@ def add_book():
     main_menu()
 
 def display_books():
-    print("Id", 3*' ',"Title", 3*' ',"Author", 3*' ',"Genre", 3*' ',"Year", 3*' ',"Price", 3*' ')
+    print("Id", 3*' ',"Title", 3*' ',"Author", 3*' ',"Genre", 3*' ',"Year", 3*' ',"Price", 3*' ',"is_available", 3*' ')
     for i in books:
-        print(i['id'], i['title'], i['author'], i['genre'], i['year'], i['price'])
-        
-    # print(books)
-    main_menu()
+        print(i['id'], ' ',i['title'], ' ',i['author'], ' ',i['genre'],  ' ',i['year'], ' ',i['is_available'])
+
+    # main_menu()
     
+def borrow():
+    display_books()
+        
+    try:
+        borrow_id = int(input("Print ID of book that you want to borrow: "))
+    except Exception as e:
+        print(e)
+    for i in books:
+        if i['id'] == borrow_id:
+            if i['is_available'] == 'Yes':
+                i['is_available'] = 'No'
+                print("Book has been borrowed")
+            else:
+                print("Book has already been borrowed")
+            break
+    else:
+        print("Book with this ID has not been found")
+            
+        
+
 
 def main_menu():
     while True:    
         print(30*'-', "MAIN MENU", 30*'-')
-        print('1. Add a book')
-        print('2. Show a book')
-        option = int(input("Enter a option that you want to "))
+        print('1. Add books')
+        print('2. Show books')
+        print('3. Borrow a book')
+        print('4. Exit')
+        try:
+            option = int(input("Enter a option (1-5) "))
+        except:
+            print("Invalid input")
+            continue
         if option == 1:
             add_book()
         elif option == 2:
             display_books()
+        elif option == 3:
+            borrow()
+        elif option == 4:
+            break
+            
+            
+            
+            
             
 main_menu()
 
